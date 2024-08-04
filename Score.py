1dbd56039f0b7b842aa365e33bd2299c31e12204
Bhuvnn <140882654+Bhuvnn@users.noreply.github.com>
2024-08-04 22:15:01 +0530

    ad

--- Score.py
+++ Score.py
@@ -5,7 +5,7 @@ class Scoreboard(Turtle):
         super().__init__()
         self.score=0
         self.high_score=0
-        with open(r"High_score.txt") as data:
+        with open(r"C:\Users\bhuva\OneDrive\Desktop\Programming Practices\00-Major Python Projects\Snake-Game\High_score.txt") as data:
             self.high_score=int(data.read())
         self.color("white")
         self.penup()  
@@ -16,7 +16,7 @@ class Scoreboard(Turtle):
     def reset(self):
         if self.score > self.high_score:
             self.high_score=self.score
-            with open("High_score.txt",mode="w") as data:
+            with open(r"C:\Users\bhuva\OneDrive\Desktop\Programming Practices\00-Major Python Projects\Snake-Game\High_score.txt",mode="w") as data:
                 data.write(f"{self.high_score}")
         self.score=0
         self.update_score()
